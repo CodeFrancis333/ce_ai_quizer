b93a532084ab6d14509e44a460154d3548e45e02
codeH <codeherrera333@gmail.com>
2025-06-23 22:37:27 +0800

    Update settings.py

--- cequiz-backend/cequiz/settings.py
+++ cequiz-backend/cequiz/settings.py
@@ -6,13 +6,11 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+# Quick-start development settings not suitable for production
+# https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-dev-key')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
